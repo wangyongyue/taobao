bb2f617897bff2c12c21e100ba3979051a80a109
wangyongyue <18601271036@163.com>
2019-12-10 21:50:52 +0800

    m

--- Demo/Configuration/Configuration.swift
+++ Demo/Configuration/Configuration.swift
@@ -16,13 +16,12 @@ let HEIGHT:CGFloat = UIScreen.main.bounds.height
 //主体颜色
 let  themeColor = UIColor.init(red: 9/255.0, green: 169/255.0, blue: 159/255.0, alpha: 1.0)
 let  bgColor = UIColor.init(red: 230/255.0, green: 230/255.0, blue: 230/255.0, alpha: 1.0)
-
 class Configuration{
-    static let instructions = KnowledgeInstructions()
+    static let instructions = TabInstructions()
     
     func getTabBar() -> UIViewController{
         let tab = BaseTabBarController()
-        tab.addChildVC(childVC: Home().v_viewController(), childTitle: "主页", image: Image.home(), selectedImage: Image.home_sel())
+        tab.addChildVC(childVC: Home().v_viewController(), childTitle: "主页", image: UIImage.init(named: "home"), selectedImage: UIImage.init(named: "home"))
         return tab
     }
     func themeColor() -> UIColor{
---
+++ Demo/Models/Tabs/Tab103.swift
@@ -0,0 +1,83 @@
+//
+//  Tab103.swift
+//  Demo
+//
+//  Created by wangyongyue on 2019/12/10.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+import VueSwift
+class Tab103:Vue,V_ViewControllerProtocol{
+    private var http = DefaultHttp()
+    
+    func v_viewController() -> UIViewController{
+        let vc = TableViewController()
+        vc.m = self
+        return vc
+    }
+    
+    override func v_start() {
+                
+        dealNav()
+        dealContent()
+      
+    }
+    private func dealNav(){
+        
+        var array = [VueData]()
+        let m = MusicHomeNavCellModel()
+        m.name = "音乐馆"
+        array.append(m)
+        self.v_array(vId: NAVARRAYID) { () -> Array<VueData>? in
+            return array
+        }
+        self.v_index(vId: NAVINDEXID) { (index) in
+            
+        }
+        
+    }
+    private func dealContent(){
+        
+       POST().request(params: self.http) { (isK, data) in
+                       
+              
+         var array = [VueData]()
+        array.append(Music101CellModel())
+        array.append(Music105CellModel())
+
+        array.append(MusicHomeCellModel("官方歌曲"))
+        array.append(Music102CellModel())
+        array.append(MusicHomeCellModel("达人歌曲"))
+        array.append(Music102CellModel())
+        array.append(MusicHomeCellModel("分类社区"))
+        array.append(Music103CellModel())
+        array.append(MusicHomeCellModel("最新专辑"))
+        array.append(Music102CellModel())
+        array.append(MusicHomeCellModel("一起听"))
+        array.append(Music104CellModel())
+        array.append(MusicHomeCellModel("VIP"))
+        array.append(Music103CellModel())
+        array.append(MusicHomeCellModel("精选视频"))
+        array.append(Music103CellModel())
+
+         self.v_array(vId: ARRAYID) { () -> Array<VueData>? in
+             return array
+                    
+         }
+                
+                  
+        }
+        
+         self.v_index(vId: INDEXID) { (index) in
+             
+            Router.push(MusicHomeDetails(), nil, nil)
+        }
+        
+    }
+  
+   
+   
+    
+}
